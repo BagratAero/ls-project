62d4e5c31f1292a9ec745e458e8b002434bc79ad
BagratAero <bagratbagratuni911@gmail.com>
2018-12-18 10:52:03 +0400

    18th 1st

--- jobs/views.py
+++ jobs/views.py
@@ -5,3 +5,8 @@ from .models import Job
 def home(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
+
+def test(request):
+    jobs = Job.objects
+    return render(request, 'jobs/test.html', {'jobs':jobs})
+
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,4 +7,5 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    path('test/', jobs.views.test, name='test'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
